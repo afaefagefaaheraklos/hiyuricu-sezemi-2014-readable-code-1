118b649a3e3a8f2f03477e3ba41670278d2409dc
kajiwara0126 <kajiwara@mtl.t.u-tokyo.ac.jp>
2014-06-22 14:47:28 +0900

    add the input to get the requested id

--- output_omuraisu.py
+++ output_omuraisu.py
@@ -6,7 +6,14 @@ import string
 
 rf = open('omuraisu_recipe_data.txt', 'r')
 
+recipe_with_id = {}
+
 id = 1
 for recipes in rf.readlines():
+	recipe_with_id['id'] = 'recipe' 
 	print (id,recipes)
 	id += 1
+
+id_requested = input("type in the id you request > ")
+print(recipe_with_id.get('id'))
+
